ddacfcf5abb49b36f7b9a272c67f03c1db20dc94
Gediminas Z <gediminas.zubovicius@gmail.com>
2025-01-16 16:29:58 +0000

    Adding remaining doubly linked list functions

--- doubly_linked_list/doubly_linked_list.go
+++ doubly_linked_list/doubly_linked_list.go
@@ -61,3 +61,186 @@ func (dll *DoublyLinkedList) Push(value string) {
 	}
 	dll.length++
 }
+
+func (dll *DoublyLinkedList) Pop() string {
+	if dll.length == 0 {
+		return ""
+	}
+
+	value := dll.tail.value
+	if dll.length == 1 {
+		dll.head = nil
+		dll.tail = nil
+	} else {
+		dll.tail = dll.tail.previousNode
+		dll.tail.nextNode = nil
+	}
+	dll.length--
+
+	return value
+}
+
+func (dll *DoublyLinkedList) Shift() string {
+	if dll.length == 0 {
+		return ""
+	}
+
+	value := dll.head.value
+	if dll.length == 1 {
+		dll.head = nil
+		dll.tail = nil
+	} else {
+		dll.head = dll.head.nextNode
+		dll.head.previousNode = nil
+	}
+	dll.length--
+
+	return value
+}
+
+func (dll *DoublyLinkedList) Unshift(value string) {
+	newNode := &Node{value: value}
+	if dll.length == 0 {
+		dll.head = newNode
+		dll.tail = newNode
+	} else {
+		dll.head.previousNode = newNode
+		newNode.nextNode = dll.head
+		dll.head = newNode
+	}
+	dll.length++
+}
+
+func (dll *DoublyLinkedList) Get(index int) string {
+	if index < 0 || index >= dll.length {
+		return ""
+	}
+
+	var currentNode *Node
+	if index < dll.length/2 {
+		currentNode = dll.head
+		for i := 0; i < index; i++ {
+			currentNode = currentNode.nextNode
+		}
+	} else {
+		currentNode = dll.tail
+		for i := dll.length - 1; i > index; i-- {
+			currentNode = currentNode.previousNode
+		}
+	}
+
+	return currentNode.value
+}
+
+func (dll *DoublyLinkedList) Set(index int, value string) bool {
+	if index < 0 || index >= dll.length {
+		return false
+	}
+
+	var currentNode *Node
+	if index < dll.length/2 {
+		currentNode = dll.head
+		for i := 0; i < index; i++ {
+			currentNode = currentNode.nextNode
+		}
+	} else {
+		currentNode = dll.tail
+		for i := dll.length - 1; i > index; i-- {
+			currentNode = currentNode.previousNode
+		}
+	}
+
+	currentNode.value = value
+
+	return true
+}
+
+func (dll *DoublyLinkedList) Insert(index int, value string) bool {
+	if index < 0 || index > dll.length {
+		return false
+	}
+
+	if index == 0 {
+		dll.Unshift(value)
+		return true
+	} else if index == dll.length {
+		dll.Push(value)
+		return true
+	}
+
+	newNode := &Node{value: value}
+	var currentNode *Node
+	if index < dll.length/2 {
+		currentNode = dll.head
+		for i := 0; i < index; i++ {
+			currentNode = currentNode.nextNode
+		}
+	} else {
+		currentNode = dll.tail
+		for i := dll.length - 1; i > index; i-- {
+			currentNode = currentNode.previousNode
+		}
+	}
+
+	newNode.nextNode = currentNode
+	newNode.previousNode = currentNode.previousNode
+	currentNode.previousNode.nextNode = newNode
+	currentNode.previousNode = newNode
+	dll.length++
+
+	return true
+}
+
+func (dll *DoublyLinkedList) DeleteByIndex(index int) string {
+	if index < 0 || index >= dll.length {
+		return ""
+	}
+
+	var currentNode *Node
+	if index < dll.length/2 {
+		currentNode = dll.head
+		for i := 0; i < index; i++ {
+			currentNode = currentNode.nextNode
+		}
+	} else {
+		currentNode = dll.tail
+		for i := dll.length - 1; i > index; i-- {
+			currentNode = currentNode.previousNode
+		}
+	}
+
+	value := currentNode.value
+	if dll.length == 1 {
+		dll.head = nil
+		dll.tail = nil
+	} else if currentNode == dll.head {
+		dll.head = dll.head.nextNode
+		dll.head.previousNode = nil
+	} else if currentNode == dll.tail {
+		dll.tail = dll.tail.previousNode
+		dll.tail.nextNode = nil
+	} else {
+		currentNode.previousNode.nextNode = currentNode.nextNode
+		currentNode.nextNode.previousNode = currentNode.previousNode
+	}
+	dll.length--
+
+	return value
+}
+
+func (dll *DoublyLinkedList) Reverse() {
+	if dll.length == 0 {
+		return
+	}
+
+	currentNode := dll.head
+	dll.head = dll.tail
+	dll.tail = currentNode
+
+	for currentNode != nil {
+		prevNode := currentNode.previousNode
+		currentNode.previousNode = currentNode.nextNode
+		currentNode.nextNode = prevNode
+		currentNode = currentNode.previousNode
+	}
+}
--- doubly_linked_list/doubly_linked_list_test.go
+++ doubly_linked_list/doubly_linked_list_test.go
@@ -3,11 +3,20 @@ package doubly_linked_list
 import "testing"
 
 func TestPush(t *testing.T) {
-	dll := NewDoublyLinkedList("A")
-	dll.Push("B")
-	dll.Push("C")
+	dll := setupDoublyLinkedList()
 
-	want := "A -> B -> C"
+	want := "A -> B -> C -> D"
+	got := dll.Print()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestPrint(t *testing.T) {
+	dll := setupDoublyLinkedList()
+
+	want := "A -> B -> C -> D"
 	got := dll.Print()
 
 	if got != want {
@@ -16,14 +25,223 @@ func TestPush(t *testing.T) {
 }
 
 func TestPrintInReverse(t *testing.T) {
-	dll := NewDoublyLinkedList("A")
-	dll.Push("B")
-	dll.Push("C")
+	dll := setupDoublyLinkedList()
 
-	want := "C -> B -> A"
+	want := "D -> C -> B -> A"
 	got := dll.PrintInReverse()
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+}
+
+func TestPop(t *testing.T) {
+	dll := setupDoublyLinkedList()
+
+	t.Run("Returns correct node value", func(t *testing.T) {
+		want := "D"
+		got := dll.Pop()
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Updates the list correctly", func(t *testing.T) {
+		want := "A -> B -> C"
+		got := dll.Print()
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func TestUnshift(t *testing.T) {
+	dll := NewDoublyLinkedList("B")
+	dll.Unshift("A")
+
+	want := "A -> B"
+	got := dll.Print()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestShift(t *testing.T) {
+	t.Run("Returns correct node value", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		want := "A"
+		got := dll.Shift()
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Updates the list correctly", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		dll.Shift()
+
+		want := "B -> C -> D"
+		got := dll.Print()
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func TestGet(t *testing.T) {
+	dll := setupDoublyLinkedList()
+
+	t.Run("Returns correct node value", func(t *testing.T) {
+		want := "C"
+		got := dll.Get(2)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Returns empty string if index is out of bounds", func(t *testing.T) {
+		want := ""
+		got := dll.Get(5)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func TestSet(t *testing.T) {
+	dll := setupDoublyLinkedList()
+
+	t.Run("Returns true if node is set", func(t *testing.T) {
+		want := true
+		got := dll.Set(2, "E")
+
+		if got != want {
+			t.Errorf("got %t, want %t", got, want)
+		}
+	})
+
+	t.Run("Updates the list correctly", func(t *testing.T) {
+		dll.Set(2, "E")
+
+		want := "A -> B -> E -> D"
+		got := dll.Print()
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Returns false if index is out of bounds", func(t *testing.T) {
+		want := false
+		got := dll.Set(5, "F")
+
+		if got != want {
+			t.Errorf("got %t, want %t", got, want)
+		}
+	})
+}
+
+func TestInsert(t *testing.T) {
+	t.Run("Returns true if node is inserted", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		want := true
+		got := dll.Insert(2, "E")
+
+		if got != want {
+			t.Errorf("got %t, want %t", got, want)
+		}
+	})
+
+	t.Run("Updates the list correctly", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		dll.Insert(2, "E")
+
+		want := "A -> B -> E -> C -> D"
+		got := dll.Print()
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Returns false if index is out of bounds", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		want := false
+		got := dll.Insert(5, "F")
+
+		if got != want {
+			t.Errorf("got %t, want %t", got, want)
+		}
+	})
+}
+
+func TestDeleteByIndex(t *testing.T) {
+	t.Run("Returns node value if node is deleted", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		want := "C"
+		got := dll.DeleteByIndex(2)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+
+	t.Run("Updates the list correctly", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		dll.DeleteByIndex(2)
+
+		want := "A -> B -> D"
+		got := dll.Print()
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Returns empty string if index is out of bounds", func(t *testing.T) {
+		dll := setupDoublyLinkedList()
+
+		want := ""
+		got := dll.DeleteByIndex(5)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
+
+func TestReverse(t *testing.T) {
+	ll := setupDoublyLinkedList()
+
+	ll.Reverse()
+
+	want := "D -> C -> B -> A"
+	got := ll.Print()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func setupDoublyLinkedList() *DoublyLinkedList {
+	dll := NewDoublyLinkedList("A")
+	dll.Push("B")
+	dll.Push("C")
+	dll.Push("D")
+
+	return dll
 }
